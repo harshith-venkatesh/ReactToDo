c99bb67905a4cab522d4d6095024cd1997c9285d
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-29 14:04:12 +0530

    Updates

---
+++ precache-manifest.819014e10baded3c6497da1757300336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966b5481d72784ad240305f68b8ce92a",
+    "url": "/Harshi7016/ReactToDo/index.html"
+  },
+  {
+    "revision": "b817ffb75611f62303d7",
+    "url": "/Harshi7016/ReactToDo/static/css/main.00f75b36.chunk.css"
+  },
+  {
+    "revision": "5369231de70aa89e3314",
+    "url": "/Harshi7016/ReactToDo/static/js/2.47293ffc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Harshi7016/ReactToDo/static/js/2.47293ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b817ffb75611f62303d7",
+    "url": "/Harshi7016/ReactToDo/static/js/main.ef3fc9fa.chunk.js"
+  },
+  {
+    "revision": "7b338c6ebb65dfffeafa",
+    "url": "/Harshi7016/ReactToDo/static/js/runtime-main.099a3c6a.js"
+  }
+]);
